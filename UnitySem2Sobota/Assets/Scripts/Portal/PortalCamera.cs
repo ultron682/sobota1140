74184b6e691c0a0a6fb5f630a386f84a00767bb0
ultron682 <52131708+ultron682@users.noreply.github.com>
2023-02-18 13:35:22 +0100

    18.02

---
+++ UnitySem2Sobota/Assets/Scripts/Portal/PortalCamera.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+    float myAngle = 0;
+
+
+    private void Update() {
+        PortalCameraController();
+    }
+
+    void PortalCameraController() {
+        Vector3 playerOffSetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = playerOffSetFromPortal + portal.position;
+
+        float angularDiffBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        if (myAngle == 90 || myAngle == 270) {
+            angularDiffBetweenPortalRotations -= 90;
+        }
+
+        Quaternion portalRotationDiff = Quaternion.AngleAxis(angularDiffBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDiff * playerCamera.forward;
+
+        if (myAngle == 90 || myAngle == 270) {
+            newCameraDirection = new Vector3(newCameraDirection.z * -1, newCameraDirection.y, newCameraDirection.x);
+
+            transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        }
+        else {
+            newCameraDirection = new Vector3(newCameraDirection.x * -1, newCameraDirection.y, newCameraDirection.z * -1);
+
+            transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        }
+
+
+
+    }
+}
