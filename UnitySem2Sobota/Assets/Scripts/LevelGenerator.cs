3e166a23a937a59574ade34e66150138a7357e0f
ultron682 <52131708+ultron682@users.noreply.github.com>
2023-04-01 13:36:01 +0200

    01.04

--- UnitySem2Sobota/Assets/Scripts/LevelGenerator.cs
+++ UnitySem2Sobota/Assets/Scripts/LevelGenerator.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour
-{
+public class LevelGenerator : MonoBehaviour {
     public Texture2D map;
     public ColorToPrefab[] colorMap;
     public float offset = 5f;
+    public Material material01;
+    public Material material02;
 
 
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     void GenerateTile(int x, int z) {
-        Color pixelColor = map.GetPixel(x,z);
+        Color pixelColor = map.GetPixel(x, z);
 
         if (pixelColor.a == 0) {
             return;
@@ -24,9 +24,34 @@ public class LevelGenerator : MonoBehaviour
         foreach (ColorToPrefab colorToPrefab in colorMap) {
             if (colorToPrefab.color == pixelColor) {
                 Vector3 positionToSpawn = new Vector3(x, 0, z) * offset;
-                Instantiate(colorToPrefab.prefab, positionToSpawn, Quaternion.identity, transform);
+                GameObject tile = Instantiate(colorToPrefab.prefab, positionToSpawn, Quaternion.identity, transform);
+                tile.transform.localPosition = positionToSpawn;
+
+                if (tile.tag == "Wall") {
+                    tile.transform.localPosition += new Vector3(0, 2.5f, 0);
+                }
+            }
+        }
+    }
+
+    public void GenerateLabirynth() {
+        for (int x = 0; x < map.width; x++) {
+            for (int y = 0; y < map.height; y++) {
+                GenerateTile(x, y);
             }
         }
 
+        ColorTheChildren();
+    }
+
+    public void ColorTheChildren() {
+        foreach (Transform child in transform) {
+            if (child.tag == "Wall") {
+                if (Random.Range(1, 100) % 3 == 0)
+                    child.gameObject.GetComponent<Renderer>().material = material02;
+                else
+                    child.gameObject.GetComponent<Renderer>().material = material01;
+            }
+        }
     }
 }
